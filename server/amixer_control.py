f6264b748b3fcff94800ce4320d906d210bb3d14
JordanGoetze <jordan.goetze@target.com>
2018-08-03 10:10:45 -0500

    add working volume control for mac

--- server/amixer_control.py
+++ server/amixer_control.py
@@ -6,19 +6,26 @@ import os
 import math
 
 
-volume_scale = 0.70
+linux_volume_scale = 0.70
+mac_volumme_scale = 1.0
+
 
 
 def set_volume(value):
     if os.name == "posix":
-        return
-    result = subprocess.run(["amixer", "-M", "set", "Master", "{}%".format(math.floor(value * volume_scale))], stdout=subprocess.PIPE)
+        subprocess.check_output("osascript -e 'set volume output volume {}'".format(value), shell=True)
+    else:
+        result = subprocess.run(["amixer", "-M", "set", "Master", "{}%".format(math.floor(value * volume_scale))], stdout=subprocess.PIPE)
 
 
 def get_volume():
     if os.name == "posix":
-        return 0
-    result = subprocess.run(["amixer", "-M", "get", "Master"], stdout=subprocess.PIPE)
-    matches = re.findall("[0-9]{1,3}%", str(result.stdout))
-    vol =  math.floor(int(matches[0].replace("%","")) / volume_scale)
-    return vol
+
+        result = subprocess.check_output("osascript -e 'output volume of (get volume settings)'", shell=True)
+        print(result)
+        return int(result)
+    else:
+        result = subprocess.run(["amixer", "-M", "get", "Master"], stdout=subprocess.PIPE)
+        matches = re.findall("[0-9]{1,3}%", str(result.stdout))
+        vol =  math.floor(int(matches[0].replace("%","")) / volume_scale)
+        return vol
